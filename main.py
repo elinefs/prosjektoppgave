38d4b63dba38c9beae4d1da6ac20aeb19ecde797
elinefs <elinefs@stud.ntnu.no>
2019-09-05 11:16:50 +0200

    Start function RunModel

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+
+
+def RunModel():
+    # ds_folder = ''
+    data_folder = '' # Folder with the interpolated data.
+    ov_file = '' # Overview file.
+    save_folder = '' # Folder to save results.
+    save_name = ''
