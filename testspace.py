692d7ceb813cffd77d44b5352b73a42053fdb0fc
elinefs <elinefs@stud.ntnu.no>
2019-10-12 18:40:28 +0200

    Change named from test to testspace.

--- testspace.py
+++ testspace.py
@@ -2,20 +2,22 @@ import sklearn
 import numpy as np
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import KFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import sklearn
 
 
 def make_partition(method):
     if method == "leave-One-Out":
         cross_validator = LeaveOneOut()
     elif method == "K-fold":
-        cross_validator = KFold(n_splits=2)
+        cross_validator = KFold(n_splits=5)
     else:
         print("Cross-validator unknown or not implemented.")
 
     return cross_validator
 
 
-data = np.array([1,2,3,4,5,6,7,8,9,10])
+data = np.array([1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15])
 cv = make_partition("K-fold")
 for train_index, test_index in cv.split(data):
     print("TRAIN:", train_index, "TEST:", test_index)
@@ -30,3 +32,8 @@ print(np.shape(data))
 
 part = data[:,0]
 print(len(np.shape(part)))
+
+x = np.linspace(0, 10, 10, endpoint=False, dtype=int)
+print(x)
+
+print(sklearn.base.is_classifier(sklearn.linear_model.SGDClassifier()))
