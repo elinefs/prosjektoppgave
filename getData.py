3bfef8fe6e9056d432d89da37b61e7d317bcdc67
elinefs <elinefs@stud.ntnu.no>
2019-10-12 18:35:22 +0200

    File for getting data

---
+++ getData.py
@@ -0,0 +1,18 @@
+import os
+
+########################################################################################################################
+
+def GetPatients(directoryPath):
+    """
+    A function to get the paths to the different patients from a directory.
+    :param directoryPath: a string with the path to the directory with the patients.
+    :return: a list with the paths to the patients.
+    """
+    patientPaths = []
+    for entry in os.listdir(directoryPath):
+        patientPath = os.path.join(directoryPath, entry)
+        if os.path.isdir(patientPath):
+            patientPaths.append(patientPath)
+    return patientPaths
+
+
