79d4d61b97da578301f226c50f1d5b164ed5f97b
elinefs <elinefs@stud.ntnu.no>
2019-10-23 20:13:01 +0200

    Add function downsample50_50

--- processData.py
+++ processData.py
@@ -28,3 +28,21 @@ def normalization(data):
     normData = scaler.transform(data)
 
     return normData
+
+def downsample50_50(patientdata, patientGroundTruth):
+    """
+    A function to down sample the data so that the dataset is balanced with 50% true and 50% false for each patient.
+    :param patientdata: a matrix with the data for one patient.
+    :param patientGroundTruth: a list with the ground truth corresponding to the patientdata.
+    :return: a down sampled matrix with the data and a corresponding down sampled list with the ground truth.
+    """
+    tumorIndex = np.where(patientGroundTruth)[0]
+    normalIndex = np.where(patientGroundTruth == False)[0]
+    number = len(tumorIndex)
+
+    keepIndex = np.random.choice(normalIndex, number, replace=False)
+
+    downsamplePatientData = np.concatenate((patientdata[tumorIndex], patientdata[keepIndex]), axis=0)
+    downsampleGroundTruth = np.concatenate((patientGroundTruth[tumorIndex], patientGroundTruth[keepIndex]), axis=0)
+
+    return downsamplePatientData, downsampleGroundTruth
