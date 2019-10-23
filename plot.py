0ec2e7178d1e5c0c90d4d5703e3c5dabe63cebc7
elinefs <elinefs@stud.ntnu.no>
2019-10-23 20:15:30 +0200

    Clean up function.

--- plot.py
+++ plot.py
@@ -2,54 +2,38 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_confusion_matrix(y_true, y_pred, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
+########################################################################################################################
+
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False):
     """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
+    A function to calculate and plot the confusion matrix.
+    :param y_true: a list with the ground truth for the test data.
+    :param y_pred: a list with the predictions for the test data.
+    :param classes: a list with names (strings) of the different classes
+    :param normalize: optional, set to True if normalization of the confusion matrix is wanted.
+    :return: a plot with the confusion matrix.
     """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
 
     # Compute confusion matrix
     cm = confusion_matrix(y_true, y_pred)
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
+        title = 'Confusion matrix, normalized'
     else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
+        title = 'Confusion matrix'
 
     fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
     ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
+    ax.set(xticks=np.arange(cm.shape[1]), yticks=np.arange(cm.shape[0]), xticklabels=classes, yticklabels=classes,
+           title=title, ylabel='True label', xlabel='Predicted label')
 
     # Loop over data dimensions and create text annotations.
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i in range(cm.shape[0]):
         for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
+            ax.text(j, i, format(cm[i, j], fmt), ha="center", va="center", color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
     return ax
