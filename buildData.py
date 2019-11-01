Correct downsampling and addfunction for probability map and binary map. Include calculation of DICE score

buildData.py
@@ -17,6 +17,7 @@ def buildDataset(patientPaths, scans, maskchoice):
     """
     dataDict = {}
     groundTruthDict = {}
+    imsizes = {}
     for patientNo, patient in enumerate(patientPaths):
 
         mask = options.radiologist(maskchoice, patient)
@@ -28,20 +29,24 @@ def buildDataset(patientPaths, scans, maskchoice):
             for index, scan in enumerate(type):
                 filename = scan + ".nii"
                 path = os.path.join(patient, filename)
-                image = processData.get_array_from_nii_image(path)
+                image, size = processData.get_array_from_nii_image(path)
                 scandata[:, index] = image
+                if patientNo not in imsizes.keys():
+                    imsizes[patientNo] = size
             scandata = processData.normalization(scandata)
             patientData.append(scandata)
 
         patientMatrix = np.concatenate(patientData, axis=1)
-        dataDict[patientNo], groundTruthDict[patientNo] = processData.downsample50_50(patientMatrix, mask)
 
-    return dataDict, groundTruthDict
+        dataDict[patientNo] = patientMatrix
+        groundTruthDict[patientNo] = mask
+
+    return dataDict, groundTruthDict, imsizes
 
 
 def get_data_for_training(dataDict, groundTruthDict, indexList):
     '''
-    A function to combine the data used to train or test the model in a dask array.
+    A function to combine the data used to train the model in a dask array.
     :param dataDict: a dictionary with the data.
     :param groundTruthDict: a dictionary with the ground truth.
     :param indexList: indexes (keys in dictionary) that should be included in the dask array.
@@ -50,8 +55,36 @@ def get_data_for_training(dataDict, groundTruthDict, indexList):
     dataList = []
     groundtruthList = []
     for index in indexList:
-        dataList.append(dataDict[index])
-        groundtruthList.append(groundTruthDict[index])
+        indexData, indexTruth = dataDict[index], groundTruthDict[index]
+
+        remove = np.where(~indexData.all(axis=1))[0]
+        indexData = np.delete(indexData, remove, 0)
+        indexTruth = np.delete(indexTruth, remove)
+
+        indexData, indexTruth = processData.downsample50_50(indexData, indexTruth)
+
+        dataList.append(indexData)
+        groundtruthList.append(indexTruth)
+
+    data = da.concatenate(dataList, axis=0)
+    groundTruth = da.concatenate(groundtruthList, axis=0)
+    return data, groundTruth
+
+
+def get_data_for_test(dataDict, groundTruthDict, indexList):
+    '''
+    A function to combine the data used to test the model in a dask array.
+    :param dataDict: a dictionary with the data.
+    :param groundTruthDict: a dictionary with the ground truth.
+    :param indexList: indexes (keys in dictionary) that should be included in the dask array.
+    :return: two dask arrays; one with data and one with ground truth.
+    '''
+    dataList = []
+    groundtruthList = []
+    for index in indexList:
+        indexData, indexTruth = dataDict[index], groundTruthDict[index]
+        dataList.append(indexData)
+        groundtruthList.append(indexTruth)
     data = da.concatenate(dataList, axis=0)
     groundTruth = da.concatenate(groundtruthList, axis=0)
     return data, groundTruth

main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 import sklearn.linear_model
+from sklearn.metrics import confusion_matrix
+import dask.array as da
 from dask_ml.wrappers import Incremental
 import time
 
@@ -9,7 +11,6 @@ import options
 import getData
 import buildData
 import plot
-from sklearn.metrics import confusion_matrix
 ########################################################################################################################
 
 t0 = time.time()
@@ -19,7 +20,7 @@ patientPaths = getData.GetPatients(basepath)
 
 # Choose which scans to include.
 t2 = ["T2"]
-dwi = ["DWI_b6"]#"DWI_b0", "DWI_b1", "DWI_b2", "DWI_b3", "DWI_b4", "DWI_b5", "DWI_b6"]
+dwi = ["DWI_b0", "DWI_b1", "DWI_b2", "DWI_b3", "DWI_b4", "DWI_b5", "DWI_b6"]
 ffe = []#"FFE_e0", "FFE_e2", "FFE_e2", "FFE_e3", "FFE_e4"]
 t1t2sense = ["T1T2SENSE_e0_t00", "T1T2SENSE_e0_t01", "T1T2SENSE_e0_t02", "T1T2SENSE_e0_t03", "T1T2SENSE_e0_t04",
              "T1T2SENSE_e0_t05", "T1T2SENSE_e0_t06", "T1T2SENSE_e0_t07", "T1T2SENSE_e0_t08", "T1T2SENSE_e0_t09",
@@ -47,18 +48,21 @@ numberOfPatients = len(patientPaths)
 patientNumbers = np.linspace(0, numberOfPatients, numberOfPatients, endpoint=False, dtype=int)
 
 # Creating dictionaries.
-dataDict, groundTruthDict = buildData.buildDataset(patientPaths, scans, maskchoice)
+dataDict, groundTruthDict, imsizes = buildData.buildDataset(patientPaths, scans, maskchoice)
 
 # Choose cross-validator.
 crossvalidator = options.select_cross_validator("K-fold", 5)
 
 loadtime = time.time()
 
+totalPred = []
+totalTruth = []
+
 # Train model.
 for train_index, test_index in crossvalidator.split(patientNumbers):
     # First splitting the data and building dask arrays.
     trainingX, trainingY = buildData.get_data_for_training(dataDict, groundTruthDict, train_index)
-    testX, testY = buildData.get_data_for_training(dataDict, groundTruthDict, test_index)
+    testX, testY = buildData.get_data_for_test(dataDict, groundTruthDict, test_index)
 
     # Rechunk to be sure to not run into memory issues later.
     trainingX = trainingX.rechunk((100000, -1))
@@ -71,12 +75,41 @@ for train_index, test_index in crossvalidator.split(patientNumbers):
     clf = Incremental(estimator, scoring='accuracy')
     clf.fit(trainingX, trainingY, classes=[True, False])
     data = clf.predict(testX)
+    #probability = clf.predict_proba(testX)
 
-    plot.plot_confusion_matrix(testY.compute(), data.compute(), classes=['Normal', 'Tumour'])
-    plt.show()
-    #print(confusion_matrix(testY.compute(), data.compute()))
+    totalPred.append(data)
+    totalTruth.append(testY)
 
+    # Per patient confusion matrix
+    index = 0
+    for patientID in test_index:
+        size = len(groundTruthDict[patientID])
+        pred = data[index:index+size].compute()
+        truth = testY[index:index+size].compute()
+        #prob = probability[index:index+size].compute()
 
+        confusionMatrix = confusion_matrix(truth, pred)
+
+        dice = (2*confusionMatrix[1,1])/((2*confusionMatrix[1,1]) + confusionMatrix[0,1] + confusionMatrix[1,0])
+        print(dice)
+
+        plot.plot_confusion_matrix(confusionMatrix, classes=['Normal', 'Tumour'], normalize=True, title="Confusion matrix, patient "+str(patientID))
+        plt.show()
+
+        plot.plot_binary_mask(pred, 10, imsizes[patientID][1:], 'Patient '+ str(patientID)+', slice 10')
+        plt.show()
+
+        index += size
+
+# Overall confusion matrix
+totalPred = da.concatenate(totalPred, axis=0)
+totalTruth = da.concatenate(totalTruth, axis=0)
+confusionMatrix = confusion_matrix(totalTruth.compute(), totalPred.compute())
+
+plot.plot_confusion_matrix(confusionMatrix, classes=['Normal', 'Tumour'])
+plt.show()
+dice = (2*confusionMatrix[1,1])/((2*confusionMatrix[1,1]) + confusionMatrix[0,1] + confusionMatrix[1,0])
+print(dice)
 t1 = time.time()
 print('loadtime: ' + str(loadtime-t0))
 print('traintime: ' + str(t1-loadtime))

options.py
@@ -36,26 +36,26 @@ def radiologist(method, patientPath):
 
     if method == "an":
         maskPath = os.path.join(patientPath, maskfile_an)
-        mask = get_array_from_nii_image(maskPath)
+        mask = get_array_from_nii_image(maskPath)[0]
 
     elif method == "shh":
         maskPath = os.path.join(patientPath, maskfile_shh)
-        mask = get_array_from_nii_image(maskPath)
+        mask = get_array_from_nii_image(maskPath)[0]
 
     elif method == "intersection":
         maskPath1 = os.path.join(patientPath, maskfile_an)
-        mask1 = get_array_from_nii_image(maskPath1)
+        mask1 = get_array_from_nii_image(maskPath1)[0]
         maskPath2 = os.path.join(patientPath, maskfile_shh)
-        mask2 = get_array_from_nii_image(maskPath2)
+        mask2 = get_array_from_nii_image(maskPath2)[0]
         mask = np.zeros(len(mask1), dtype=np.bool)
         for i in range(len(mask1)):
             mask[i] = mask1[i] and mask2[i]
 
     elif method == "union":
         maskPath1 = os.path.join(patientPath, maskfile_an)
-        mask1 = get_array_from_nii_image(maskPath1)
+        mask1 = get_array_from_nii_image(maskPath1)[0]
         maskPath2 = os.path.join(patientPath, maskfile_shh)
-        mask2 = get_array_from_nii_image(maskPath2)
+        mask2 = get_array_from_nii_image(maskPath2)[0]
         mask = np.zeros(len(mask1), dtype=np.bool)
         for i in range(len(mask1)):
             mask[i] = mask1[i] or mask2[i]

plot.py
@@ -4,24 +4,19 @@ import numpy as np
 
 ########################################################################################################################
 
-def plot_confusion_matrix(y_true, y_pred, classes, normalize=False):
+def plot_confusion_matrix(cm, classes, normalize=False, title="Overall confusion matrix"):
     """
     A function to calculate and plot the confusion matrix.
-    :param y_true: a list with the ground truth for the test data.
-    :param y_pred: a list with the predictions for the test data.
+    :param cm: the confusion matrix.
     :param classes: a list with names (strings) of the different classes
     :param normalize: optional, set to True if normalization of the confusion matrix is wanted.
+    :param title: optional, a string with the title of the plot.
     :return: a plot with the confusion matrix.
     """
 
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        title = 'Confusion matrix, normalized'
-    else:
-        title = 'Confusion matrix'
+
 
     fig, ax = plt.subplots()
     im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
@@ -36,4 +31,45 @@ def plot_confusion_matrix(y_true, y_pred, classes, normalize=False):
         for j in range(cm.shape[1]):
             ax.text(j, i, format(cm[i, j], fmt), ha="center", va="center", color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
+    return ax
+
+
+def plot_probability_map(patientprob, slice, imsize, title):
+    '''
+
+    :param patientprob:
+    :param slice:
+    :param imsize:
+    :param title:
+    :return:
+    '''
+    startIndex = slice * imsize[0] * imsize[1]
+    stopIndex = startIndex + (imsize[0] * imsize[1])
+
+    prob = np.reshape(patientprob[startIndex:stopIndex, 1], (imsize[0], imsize[1]))
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(prob, interpolation='nearest', cmap='hot')
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(title=title)
+    return ax
+
+
+def plot_binary_mask(predData, slice, imsize, title):
+    '''
+
+    :param predData:
+    :param slice:
+    :param imsize:
+    :param title:
+    :return:
+    '''
+    startIndex = slice * imsize[0] * imsize[1]
+    stopIndex = startIndex + (imsize[0] * imsize[1])
+
+    pred = np.reshape(predData[startIndex:stopIndex], (imsize[0], imsize[1]))
+
+    fig, ax = plt.subplots()
+    ax.imshow(pred)
+    ax.set(title=title)
     return ax

processData.py
@@ -12,8 +12,9 @@ def get_array_from_nii_image(path):
     """
     image = sitk.ReadImage(path)
     array = sitk.GetArrayFromImage(image)
+    imsize = np.shape(array)
     array = array.flatten()
-    return array
+    return array, imsize
 
 def normalization(data):
     """